130dd3620986bd70343a181147d1d63a6e78dd1e
892280082 <892280082@qq.com>
2022-04-09 12:19:10 +0800

    auto update

--- index.js
+++ index.js
@@ -3,31 +3,36 @@ const {readLine,readNumber} = require('./src/userInput')
 const {ExtendsModuleTemplateMove} = require("./src/ProjectMove");
 const path =require('path');
 
-
-
-const main = async ()=>{
-
-	const projectName = await readLine("please enter a project/module name,using the camel name rule! ")
-
-	const projectType = await readNumber("please enter the creation type: \n 1.spring-ioc native \n 2.spring-mvc for web \n 3.spring-ioc-module",1,3)
-
-	const MvClass = [null,null,ExtendsModuleTemplateMove][projectType-1];
-
-	if(!MvClass){
-		console.log("It has not been developed yet");
-		return;
-	}
-
-	const mvInstance = new MvClass(projectName)
+/**
+	to instance specified class and copy to destnation.
+*/
+const copyTemplate = async (classConstructor,projectName) => {
 
 	const cwdPath = process.cwd();
 
-	//format directory name
+	const mvInstance = new classConstructor(projectName)
+
 	const desnaPath = path.join(cwdPath,mvInstance.getFormatDirName())
 
 	await mvInstance.copyTo(desnaPath)
 
 	await mvInstance.modify();
+
+	return mvInstance;
+}
+
+
+const main = async ()=>{
+
+	const projectType = await readNumber("please enter the building type: \n 1.spring-ioc native \n 2.spring-mvc for web \n 3.spring-ioc-module",1,3)
+
+	const tips = ["","","please enter a java Bean name. example: HelloWorldBean "];
+
+	const classConstructorList = [null,null,ExtendsModuleTemplateMove]
+
+	const projectName = await readLine(tips[projectType-1])
+
+	const mvInstance = await copyTemplate(classConstructorList[projectType-1],projectName);
 
 	console.log(`Building complish!  \n 1.cd ${mvInstance.getFormatDirName()} \n 2.npm install \n 3.node index.js`);
 }
